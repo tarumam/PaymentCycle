f0aac66ef8f24bb29b5e3bb8294b7ca9186f0dcc
tarumam <tarumamiranda@gmail.com>
2018-02-01 17:16:48 -0200

    Inicio aula 119

--- FrontEnd/src/index.jsx
+++ FrontEnd/src/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './main/app'
+import reducers from './main/reducers'
+import promise from 'redux-promise'
 
+const store = applyMiddleware(promise)(createStore)(reducers)
 ReactDOM.render(
-
-    <App />
-
+    <Provider store={store}>
+        <App />
+    </Provider>
     , document.getElementById('app'))
