d33868873c9f8698b6be6737bd6be0d73ba43489
tarumam <tarumamiranda@gmail.com>
2018-02-01 14:48:07 -0200

    Estrutura da aplicação montada - Começando aula 111

---
+++ FrontEnd/src/common/template/sideBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Menu from './menu'
+
+export default props =>
+    <aside className='main-sidebar'>
+        <section className='sidebar'>
+            <Menu />
+        </section>
+    </aside>
